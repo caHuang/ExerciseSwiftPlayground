9ea6afe9f8213b48f9588742da3ce6324596036f
Yolk Huang <yolk.tw@gmail.com>
2016-04-14 16:58:02 +0800

    Add "Generics" page

---
+++ A Swift Tour.playground/Pages/Generics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+//: [Previous](@previous)
+
+//: Generics
+
+func repeatItem<Item> (item: Item, numberOfTimes: Int) -> [Item] {
+  var result = [Item]()
+  for _ in 0 ..< numberOfTimes {
+    result.append(item)
+  }
+  return result
+}
+repeatItem("knock", numberOfTimes: 4)
+
+enum OptionalValue<Wrapped> {
+  case None
+  case Some(Wrapped)
+}
+var possibleInteger: OptionalValue<Int> = .None
+possibleInteger = .Some(100)
+
+func anyCommonElements<T: SequenceType, U: SequenceType where T.Generator.Element: Equatable, T.Generator.Element == U.Generator.Element> (lhs: T, _ rhs: U) -> Bool {
+  for lhsItem in lhs {
+    for rhsItem in rhs {
+      if lhsItem == rhsItem {
+        return true
+      }
+    }
+  }
+  return false
+}
+anyCommonElements([1, 2, 3], [3])

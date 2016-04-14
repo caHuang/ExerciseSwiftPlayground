2e5be9029bef2d76a5c507bf730ed89871402a9d
Yolk Huang <yolk.tw@gmail.com>
2016-04-14 18:17:50 +0800

    Add "Constants and Variables" page

---
+++ The Basics.playground/Pages/Constants and Variables .xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+//: Constants and Variables
+
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+var welcomeMessage: String
+welcomeMessage = "Hello"
+
+var red, green, blue: Double
+
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+
+let languageName = "Swift"
+
+print(friendlyWelcome)
+print("The current value of friendlyWelcome is \(friendlyWelcome)")
+
+//: [Next](@next)

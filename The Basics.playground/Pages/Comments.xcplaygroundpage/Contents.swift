06635b18373b463ae76b2bc268d21900ace1665b
Yolk Huang <yolk.tw@gmail.com>
2016-04-14 18:25:51 +0800

    Add "Comments" page

---
+++ The Basics.playground/Pages/Comments.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+
+//: Comments
+
+// this is a comment
+
+/* this is also a comment,
+ but written over multiple lines */
+
+/* this is the start of the first multiline comment
+ /* this is the second, nested multiline comment */
+ this is the end of the first multiline comment */
+
+//: [Next](@next)

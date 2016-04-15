5622882320bc107e7ba9a1ddd29c7b63bd3972b9
Yolk Huang <yolk.tw@gmail.com>
2016-04-15 10:45:26 +0800

    Add "Integers" page

---
+++ The Basics.playground/Pages/Integers.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+
+//:　Integers
+//: * Int
+//:   * On a 32-bit platform, Int is the same size as Int32.
+//:   * On a 64-bit platform, Int is the same size as Int64.
+//: * UInt
+//:   * On a 32-bit platform, UInt is the same size as UInt32.
+//:   * On a 64-bit platform, UInt is the same size as UInt64.
+
+let minValue = UInt8.min
+let maxValue = UInt8.max
+
+//: [Next](@next)

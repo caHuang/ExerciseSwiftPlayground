9d547d78f29b4699802a67658d99f28211682c21
Yolk Huang <yolk.tw@gmail.com>
2016-04-21 17:39:58 +0800

    Add "Booleans" page

---
+++ The Basics.playground/Pages/Booleans.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: [Previous](@previous)
+
+let orangesAreOrange = true
+let turnipsAreDelicious = false
+
+if turnipsAreDelicious {
+  print("Mmm, tasty turnips!")
+} else {
+  print("Eww, turnips are horrible.")
+}
+
+
+//let i = 1
+//if i {
+//  // this example will not compile, and will report an error
+//}
+
+let i = 1
+if i == 1 {
+  // this example will compile successfully
+}
+
+//: [Next](@next)

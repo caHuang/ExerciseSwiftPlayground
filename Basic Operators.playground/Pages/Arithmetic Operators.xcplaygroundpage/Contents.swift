95c1607615cff6a6ba3b40cb6d983edc94d5a0bb
Yolk Huang <yolk.tw@gmail.com>
2016-04-22 16:09:45 +0800

    Add "Assignment Operator", "Arithmetic Operators" pages

---
+++ Basic Operators.playground/Pages/Arithmetic Operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+//: [Previous](@previous)
+
+1 + 2
+5 - 3
+2 * 3
+10.0 / 2.5
+
+"hello, " + "world"
+
+9 % 4
+-9 % 4
+
+var i = 0
+++i
+
+var a = 0
+let b = ++a
+let c = a++
+
+let three = 3
+let minusThree = -three
+let plusThree = -minusThree
+
+let minusSix = -6
+let alsoMinusSix = +minusSix
+
+//: [Next](@next)
---
+++ Basic Operators.playground/Pages/Assignment Operator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+let b = 10
+var a = 5
+a = b
+
+let (x, y) = (1, 2)
+
+//if x = y {
+//  // this is not valid, because x = y does not return a value
+//}
+
+//: [Next](@next)

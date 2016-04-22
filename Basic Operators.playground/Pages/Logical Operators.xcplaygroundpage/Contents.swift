d80fde7f62ccf7b47d4fe60a9b69f4216d8e315c
Yolk Huang <yolk.tw@gmail.com>
2016-04-22 18:26:04 +0800

    Add "Logical Operators" page

---
+++ Basic Operators.playground/Pages/Logical Operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+//: [Previous](@previous)
+
+let allowedEntry = false
+if !allowedEntry {
+  print("ACCESS DENIED")
+}
+
+let enteredDoorCode = true
+let passedRetinaScan = false
+if enteredDoorCode && passedRetinaScan {
+  print("Welcome!")
+} else {
+  print("ACCESS DENIED")
+}
+
+let hasDoorKey = false
+let knowsOverridePassword = true
+if hasDoorKey || knowsOverridePassword {
+  print("Welcome!")
+} else {
+  print("ACCESS DENIED")
+}
+
+if enteredDoorCode && passedRetinaScan || hasDoorKey || knowsOverridePassword {
+  print("Welcome!")
+} else {
+  print("ACCESS DENIED")
+}
+
+//: [Next](@next)

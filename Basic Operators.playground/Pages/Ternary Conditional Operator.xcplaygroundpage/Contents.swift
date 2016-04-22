0f8e3ead8937b1735b0e10073c93b256903eb4a8
Yolk Huang <yolk.tw@gmail.com>
2016-04-22 17:28:14 +0800

    Add "Ternary Conditional Operator" page

---
+++ Basic Operators.playground/Pages/Ternary Conditional Operator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+
+let contentHeight = 40
+let hasHeader = true
+let rowHeight = contentHeight + (hasHeader ? 50 : 20)
+
+//let contentHeight = 40
+//let hasHeader = true
+//var rowHeight = contentHeight
+//if hasHeader {
+//  rowHeight = rowHeight + 50
+//} else {
+//  rowHeight = rowHeight + 20
+//}
+
+//: [Next](@next)

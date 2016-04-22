6d0fc24352ea50a04e8abadcb284b2e6d790cdc3
Yolk Huang <yolk.tw@gmail.com>
2016-04-22 13:24:19 +0800

    Add "Optionals" page

---
+++ The Basics.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -0,0 +1,45 @@
+//: [Previous](@previous)
+
+let possibleNumber = "123"
+let convertedNumber = Int(possibleNumber)
+
+var serverResponseCode: Int? = 404
+serverResponseCode = nil
+
+var surveyAnswer: String?
+
+if convertedNumber != nil {
+  print("convertedNumber contains some integer value.")
+}
+
+if convertedNumber != nil {
+  print("convertedNumber has an integer value of \(convertedNumber!).")
+}
+
+
+if let actualNumber = Int(possibleNumber) {
+  print("\'\(possibleNumber)\' has an integer value of \(actualNumber)")
+} else {
+  print("\'\(possibleNumber)\' could not be converted to an integer")
+}
+
+
+if let firstNumber = Int("4"), secondNumber = Int("42") where firstNumber < secondNumber {
+  print("\(firstNumber) < \(secondNumber)")
+}
+
+let possibleString: String? = "An optional string."
+let forcedString: String = possibleString!
+
+let assumedString: String! = "An implicitly unwrapped optional string."
+let implicitString: String = assumedString
+
+if assumedString != nil {
+  print(assumedString)
+}
+
+if let definiteString = assumedString {
+  print(definiteString)
+}
+
+//: [Next](@next)

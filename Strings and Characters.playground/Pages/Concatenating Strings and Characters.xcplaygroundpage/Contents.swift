e7ba09491bfecbd0aef9e3de6ab873d39b71b69d
Yolk Huang <yolk.tw@gmail.com>
2016-04-24 01:07:38 +0800

    Add "Concatenating Strings and Characters" page

---
+++ Strings and Characters.playground/Pages/Concatenating Strings and Characters.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+let string1 = "hello"
+let string2 = " there"
+var welcome = string1 + string2
+
+var instruction = "look over"
+instruction += string2
+
+let exclamationMark: Character = "!"
+welcome.append(exclamationMark)
+
+//: [Next](@next)

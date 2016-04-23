1d0537e24bd25f3fc5bfc70c2ffa42ca1791fc11
Yolk Huang <yolk.tw@gmail.com>
2016-04-24 01:30:28 +0800

    Add "Unicode" page

---
+++ Strings and Characters.playground/Pages/Unicode.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+let wiseWords = "\"Imagination is more important than knowledge\" - Einstein"
+
+let dollarSign = "\u{24}"
+let blackHeart = "\u{2665}"
+let sparklingHeart = "\u{1F496}"
+
+let eAcute: Character = "\u{E9}"
+let combinedEAcute: Character = "\u{65}\u{301}"
+
+let precomposed: Character = "\u{D55C}"
+let decomposed: Character = "\u{1112}\u{1161}\u{11AB}"
+
+let enclosedEAcute: Character = "\u{E9}\u{20DD}"
+
+let regionalIndicatorForUS: Character = "\u{1F1FA}\u{1F1F8}"
+
+//: [Next](@next)

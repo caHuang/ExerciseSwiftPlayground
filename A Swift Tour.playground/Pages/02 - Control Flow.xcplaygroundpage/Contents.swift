00908eb1a024aa7876ea4aab419cdae8f986eb10
Yolk Huang <yolk.tw@gmail.com>
2016-04-26 00:26:21 +0800

    Rename "A Swift Tour" playground's pages

--- A Swift Tour.playground/Pages/02 - Control Flow.xcplaygroundpage/Contents.swift
+++ A Swift Tour.playground/Pages/02 - Control Flow.xcplaygroundpage/Contents.swift
@@ -68,14 +68,9 @@ repeat {
 print(m)
 
 var firstForLoop = 0
-for i in 0 ..< 4 {
+for i in 0..<4 {
   firstForLoop += i
 }
 print(firstForLoop)
 
-var secondForLoop = 0
-for var i = 0; i < 4; ++i {
-  secondForLoop += i
-}
-print(secondForLoop)
 //: [Next](@next)

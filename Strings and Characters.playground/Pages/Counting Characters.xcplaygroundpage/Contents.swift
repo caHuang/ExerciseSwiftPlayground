eac7b18822fae872e57a848102100f900729cecc
Yolk Huang <yolk.tw@gmail.com>
2016-04-25 21:25:38 +0800

    Add "Counting Characters" page

---
+++ Strings and Characters.playground/Pages/Counting Characters.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+let unusualMenagerie = "Koala 🐨, Snail 🐌, Penguin 🐧, Dromedary 🐪"
+print("unusualMenagerie has \(unusualMenagerie.characters.count) characters")
+
+var word = "cafe"
+print("the number of characters in \(word) is \(word.characters.count)")
+
+word += "\u{301}"
+print("the number of characters in \(word) is \(word.characters.count)")
+
+//: [Next](@next)

91b0fb9594e2d0851a11c4f5eda7e66455f4d381
Yolk Huang <yolk.tw@gmail.com>
2016-04-26 16:15:53 +0800

    Add "For-In Loops" page

---
+++ 06 - Control Flow.playground/Pages/01 - For-In Loops.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+//: [Previous](@previous)
+
+for index in 1...5 {
+ print("\(index) times 5 is \(index * 5)")
+}
+
+let base = 3
+let power = 10
+var answer = 1
+for _ in 1...power {
+  answer *= base
+}
+print("\(base) to the power of \(power) is \(answer)")
+
+let names = ["Anna", "Alex", "Brian", "Jack"]
+for name in names {
+  print("Hello, \(name)!")
+}
+
+let numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
+for (animalName, legCount) in numberOfLegs {
+  print("\(animalName)s have \(legCount) legs")
+}
+
+//: [Next](@next)

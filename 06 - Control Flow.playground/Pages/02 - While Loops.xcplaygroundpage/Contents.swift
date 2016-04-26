8650c660975a3c34166f5f7241f2ade5b62a7cab
Yolk Huang <yolk.tw@gmail.com>
2016-04-26 17:32:11 +0800

    Add "While Loops" page

---
+++ 06 - Control Flow.playground/Pages/02 - While Loops.xcplaygroundpage/Contents.swift
@@ -0,0 +1,40 @@
+//: [Previous](@previous)
+
+let finalSquare = 25
+var board = [Int](count: finalSquare + 1, repeatedValue: 0)
+
+board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+
+var square = 0
+var diceRoll = 0
+
+while square < finalSquare {
+  diceRoll += 1
+  if diceRoll == 7 {
+    diceRoll = 1
+  }
+  square += diceRoll
+  if square < board.count {
+    square += board[square]
+  }
+}
+print("Game over!")
+
+
+//var square = 0
+//var diceRoll = 0
+square = 0
+diceRoll = 0
+
+repeat {
+  square += board[square]
+  diceRoll += 1
+  if diceRoll == 7 {
+    diceRoll = 1
+  }
+  square += diceRoll
+} while square < finalSquare
+print("Game over!")
+
+//: [Next](@next)

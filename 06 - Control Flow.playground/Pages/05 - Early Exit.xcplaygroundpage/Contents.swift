a0a717796b32e7c467f548a0cd58c779203bbe2c
Yolk Huang <yolk.tw@gmail.com>
2016-04-27 15:25:59 +0800

    Add "Early Exit" page

---
+++ 06 - Control Flow.playground/Pages/05 - Early Exit.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+func greet(person: [String: String]) {
+  guard let name = person["name"] else {
+    return
+  }
+  print("Hello \(name)!")
+  
+  guard let location = person["location"] else {
+    print("I hope the weather is nice near you.")
+    return
+  }
+  print("I hope the weather is nice in \(location).")
+}
+
+greet(["name": "John"])
+greet(["name": "Jane", "location": "Cupertino"])
+
+//: [Next](@next)

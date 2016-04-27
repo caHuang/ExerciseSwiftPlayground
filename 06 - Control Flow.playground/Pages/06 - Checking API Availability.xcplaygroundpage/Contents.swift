363ea485bbb7f8382676e21c31ece2a72bf57de2
Yolk Huang <yolk.tw@gmail.com>
2016-04-27 16:24:12 +0800

    Add "Checking API Availability" page

---
+++ 06 - Control Flow.playground/Pages/06 - Checking API Availability.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+//: [Previous](@previous)
+
+if #available(iOS 9, OSX 10.10, *) {
+  // Use iOS 9 APIs on iOS, and use OS X v10.10 APIs on OS X
+} else {
+  // Fall back to earlier iOS and OS X APIs
+}
+
+//: [Next](@next)

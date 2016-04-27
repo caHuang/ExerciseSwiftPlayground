38d4c92ca067ed77715c7115cda862dd35f65122
Yolk Huang <yolk.tw@gmail.com>
2016-04-27 11:45:43 +0800

    Add "Conditional Statements" page

---
+++ 06 - Control Flow.playground/Pages/03 - Conditional Statements.xcplaygroundpage/Contents.swift
@@ -0,0 +1,114 @@
+//: [Previous](@previous)
+
+var temperatureInFahrenheit = 30
+if temperatureInFahrenheit <= 32 {
+  print("It's very cold. Consider wearing a scarf.")
+}
+
+temperatureInFahrenheit = 40
+if temperatureInFahrenheit <= 32 {
+  print("It's very cold. Consider wearing a scarf.")
+} else {
+  print("It's not that cold. Wear a t-shirt.")
+}
+
+temperatureInFahrenheit = 90
+if temperatureInFahrenheit <= 32 {
+  print("It's very cold. Consider wearing a scarf.")
+} else if temperatureInFahrenheit >= 86 {
+  print("It's really warm. Don't forget to wear sunscreen.")
+} else {
+  print("It's not that cold. Wear a t-shirt.")
+}
+
+temperatureInFahrenheit = 72
+if temperatureInFahrenheit <= 32 {
+  print("It's very cold. Consider wearing a scarf.")
+} else if temperatureInFahrenheit >= 86 {
+  print("It's really warm. Don't forget to wear sunscreen.")
+}
+
+let someCharacter: Character = "e"
+switch someCharacter {
+case "a", "e", "i", "o", "u":
+  print("\(someCharacter) is a vowel")
+case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z":
+  print("\(someCharacter) is a consonant")
+default:
+  print("\(someCharacter) is not a vowel or a consonant")
+}
+
+//: case "a": does not contain any executable statements.
+//let anotherCharacter: Character = "a"
+//switch anotherCharacter {
+//case "a":
+//case "A":
+//  print("The letter A")
+//default:
+//  print("Not the letter A")
+//}
+let anotherCharacter: Character = "a"
+switch anotherCharacter {
+case "a", "A":
+  print("The letter A")
+default:
+  print("Not the letter A")
+}
+
+let approximateCount = 62
+let countedThings = "moons orbiting Saturn"
+var naturalCount: String
+switch approximateCount {
+case 0:
+  naturalCount = "no"
+case 1..<5:
+  naturalCount = "a few"
+case 5..<12:
+  naturalCount = "several"
+case 12..<100:
+  naturalCount = "dozens of"
+case 100..<1000:
+  naturalCount = "hundreds of"
+default:
+  naturalCount = "many"
+}
+print("There are \(naturalCount) \(countedThings).")
+
+
+let somePoint = (1, 1)
+switch somePoint {
+case (0, 0):
+  print("(0, 0) is at the origin")
+case (_, 0):
+  print("(\(somePoint.0), 0) is on the x-axis")
+case (0, _):
+  print("(0, \(somePoint.1)) is on the y-axis")
+case (-2...2, -2...2):
+  print("(\(somePoint.0), \(somePoint.1)) is inside the box")
+default:
+  print("(\(somePoint.0), \(somePoint.1)) is outside of the box")
+}
+
+//: This switch statement does not have a default case. The final case, case let (x, y), declares a tuple of two placeholder constants that can match any value. As a result, this case matches all possible remaining values, and a default case is not needed to make the switch statement exhaustive.
+let anotherPoint = (2, 0)
+switch anotherPoint {
+case (let x, 0):
+  print("on the x-axis with an x value of \(x)")
+case (0, let y):
+  print("on the y-axis with a y value of \(y)")
+case let (x, y):
+  print("somewhere else at (\(x), \(y))")
+}
+
+let yetAnotherPoint = (1, -1)
+switch yetAnotherPoint {
+case let (x, y) where x == y:
+  print("(\(x), \(y)) is on the line x == y")
+case let (x, y) where x == -y:
+  print("(\(x), \(y)) is on the line x == -y")
+case let (x, y):
+  print("(\(x), \(y)) is just some arbitrary point")
+}
+
+
+//: [Next](@next)

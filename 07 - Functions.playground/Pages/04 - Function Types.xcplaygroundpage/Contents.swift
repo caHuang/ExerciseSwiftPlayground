40fdc23ab85df8aa854cc0ae99f42a4ba2fa6220
Yolk Huang <yolk.tw@gmail.com>
2016-04-29 23:47:01 +0800

    Add "Function Types" page

---
+++ 07 - Functions.playground/Pages/04 - Function Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,54 @@
+//: [Previous](@previous)
+
+func addTwoInts(a: Int, _ b: Int) -> Int {
+  return a + b
+}
+
+func multiplyTwoInts(a: Int, _ b: Int) -> Int {
+  return a * b
+}
+
+func printHelloWorld() {
+  print("hello, world")
+}
+
+var mathFunction: (Int, Int) -> Int = addTwoInts
+print("Result: \(mathFunction(2, 3))")
+
+mathFunction = multiplyTwoInts
+print("Result: \(mathFunction(2, 3))")
+
+let anotherMathFunction = addTwoInts
+
+
+func printMathResult(mathFunction: (Int, Int) -> Int, _ a: Int, _ b: Int) {
+  print("Result: \(mathFunction(a, b))")
+}
+printMathResult(addTwoInts, 3, 5)
+
+
+func stepForward(input: Int) -> Int {
+  return input + 1
+}
+
+func stepBackward(input: Int) -> Int {
+  return input - 1
+}
+
+
+func chooseStepFunction(backwards: Bool) -> (Int) -> Int {
+  return backwards ? stepBackward : stepForward
+}
+
+var currentValue = 3
+let moveNearerToZero = chooseStepFunction(currentValue > 0)
+
+
+print("Counting to zero:")
+while currentValue != 0 {
+  print("\(currentValue)... ")
+  currentValue = moveNearerToZero(currentValue)
+}
+print("zero!")
+
+//: [Next](@next)

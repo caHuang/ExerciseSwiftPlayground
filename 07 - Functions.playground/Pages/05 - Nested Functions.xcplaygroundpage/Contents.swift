90b7ff94edf2ba2d4d72fb8b72e902907d2b97b1
Yolk Huang <yolk.tw@gmail.com>
2016-04-30 22:03:35 +0800

    Add "Nested Functions" page

---
+++ 07 - Functions.playground/Pages/05 - Nested Functions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+func chooseStepFunction(backwards: Bool) -> (Int) -> Int {
+  func stepForward(input: Int) -> Int { return input + 1 }
+  func stepBackward(input: Int) -> Int { return input - 1 }
+  return backwards ? stepBackward : stepForward
+}
+
+var currentValue = -4
+
+let moveNearerToZero = chooseStepFunction(currentValue > 0)
+
+while currentValue != 0 {
+  print("\(currentValue)... ")
+  currentValue = moveNearerToZero(currentValue)
+}
+print("zero!")
+
+//: [Next](@next)

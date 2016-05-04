70e825099163cc1d76019ef4f4dd83cfbcd4cfa0
Yolk Huang <yolk.tw@gmail.com>
2016-05-04 23:32:01 +0800

    Add "Enumeration Syntax" page

---
+++ 09 - Enumerations.playground/Pages/01 - Enumeration Syntax.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+enum CompassPoint {
+  case North
+  case South
+  case East
+  case West
+}
+
+enum Planet {
+  case Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
+}
+
+var directionToHead = CompassPoint.West
+directionToHead = .East
+
+//: [Next](@next)

d947aee47dbd4a99756f14bfb59b61835d0cf2e1
Yolk Huang <yolk.tw@gmail.com>
2016-05-16 23:38:03 +0800

    Add "Defining a Base Class" page

---
+++ 14 - Inheritance.playground/Pages/01 - Defining a Base Class.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+class Vehicle {
+  var currentSpeed = 0.0
+  var description: String {
+    return "traveling at \(currentSpeed) miles per hour"
+  }
+  func makeNoise() {
+    // do nothing - an arbitrary vehicle doesn't necessarily make a noise
+  }
+}
+
+let someVehicle = Vehicle()
+
+print("Vehicle: \(someVehicle.description)")
+
+//: [Next](@next)

737c35a136e13d74f545e1fa3757c15905845774
Yolk Huang <yolk.tw@gmail.com>
2016-05-17 00:05:24 +0800

    Add "Subclassing" page

---
+++ 14 - Inheritance.playground/Pages/02 - Subclassing.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+//: [Previous](@previous)
+
+class Vehicle {
+  var currentSpeed = 0.0
+  var description: String {
+    return "traveling at \(currentSpeed) miles per hour"
+  }
+  func makeNoise() {
+    // do nothing - an arbitrary vehicle doesn't necessarily make a noise
+  }
+}
+
+
+class Bicycle: Vehicle {
+  var hasBasket = false
+}
+
+let bicycle = Bicycle()
+bicycle.hasBasket = true
+
+bicycle.currentSpeed = 15.0
+print("Bicycle: \(bicycle.description)")
+
+
+class Tandem: Bicycle {
+  var currentNumberOfPassengers = 0
+}
+
+let tandem = Tandem()
+tandem.hasBasket = true
+tandem.currentNumberOfPassengers = 2
+tandem.currentSpeed = 22.0
+print("Tandem: \(tandem.description)")
+
+//: [Next](@next)

5da54395355e2a0a617f7af6e36a03c9f78b5a65
Yolk Huang <yolk.tw@gmail.com>
2016-05-30 23:36:23 +0800

    Add "Resolving Strong Reference Cycles Between Class Instances" page

---
+++ 17 - Automatic Reference Counting.playground/Pages/03 - Resolving Strong Reference Cycles Between Class Instances.xcplaygroundpage/Contents.swift
@@ -0,0 +1,77 @@
+//: [Previous](@previous)
+
+class Person {
+  let name: String
+  init(name: String) { self.name = name }
+  var apartment: Apartment?
+  deinit { print("\(name) is being deinitialized") }
+}
+
+class Apartment {
+  let unit: String
+  init(unit: String) { self.unit = unit }
+  weak var tenant: Person?
+  deinit { print("Apartment \(unit) is being deinitialized") }
+}
+
+var john: Person?
+var unit4A: Apartment?
+
+john = Person(name: "John Appleseed")
+unit4A = Apartment(unit: "4A")
+
+john!.apartment = unit4A
+unit4A!.tenant = john
+
+john = nil
+unit4A = nil
+
+class Customer {
+  let name: String
+  var card: CreditCard?
+  init(name: String) {
+    self.name = name
+  }
+  deinit { print("\(name) is being deinitialized") }
+}
+
+class CreditCard {
+  let number: UInt64
+  unowned let customer: Customer
+  init(number: UInt64, customer: Customer) {
+    self.number = number
+    self.customer = customer
+  }
+  deinit { print("Card #\(number) is being deinitialized") }
+}
+
+var johnA: Customer?
+johnA = Customer(name: "John Appleseed")
+johnA!.card = CreditCard(number: 1234_5678_9012_3456, customer: johnA!)
+
+johnA = nil
+
+
+class Country {
+  let name: String
+  var capitalCity: City!
+  init(name: String, capitalName: String) {
+    self.name = name
+    self.capitalCity = City(name: capitalName, country: self)
+  }
+}
+
+class City {
+  let name: String
+  unowned let country: Country
+  init(name: String, country: Country) {
+    self.name = name
+    self.country = country
+  }
+}
+
+var country = Country(name: "Canada", capitalName: "Ottawa")
+print("\(country.name)'s capital city is called \(country.capitalCity.name)")
+
+//: [Next](@next)
+

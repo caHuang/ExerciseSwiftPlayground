02873a8f8d15e5d878ed21386f11d31e59bab18d
Yolk Huang <yolk.tw@gmail.com>
2016-06-09 21:23:14 +0800

    Add "Specifying Cleanup Actions" page

---
+++ 19 - Error Handling.playground/Pages/03 - Specifying Cleanup Actions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+
+//func processFile(filename: String) throws {
+//  if exists(filename) {
+//    let file = open(filename)
+//    defer {
+//      close(file)
+//    }
+//    while let line = try file.readline() {
+//      // Work with the file.
+//    }
+//    // close(file) is called here, at the end of the scope.
+//  }
+//}
+
+//: [Next](@next)

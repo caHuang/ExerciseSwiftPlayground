efa94c9e6c8b24fbe2fa3dc193f770cb6cfd517a
Yolk Huang <yolk.tw@gmail.com>
2018-12-11 23:52:17 +0800

    Update to 4.2 version - Functions and Closures

--- 01 - A Swift Tour.playground/Pages/03 - Functions and Closures.xcplaygroundpage/Contents.swift
+++ 01 - A Swift Tour.playground/Pages/03 - Functions and Closures.xcplaygroundpage/Contents.swift
@@ -1,14 +1,19 @@
 //: [Previous](@previous)
 
 //: Functions and Closures
-func greet(name: String, day: String) -> String {
-  return "Hello \(name), today is \(day)"
+func greet(person: String, day: String) -> String {
+  return "Hello \(person), today is \(day)."
 }
-greet("Bob", day: "Tuesday")
+greet(person: "Bob", day: "Tuesday")
+
+func greet(_ person: String, on day: String) -> String {
+  return "Hello \(person), today is \(day)."
+}
+greet("John", on: "Wednesday")
 
 func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
   var min = scores[0]
-  var max = scores[1]
+  var max = scores[0]
   var sum = 0
   
   for score in scores {
@@ -23,22 +28,22 @@ func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
   return (min, max, sum)
 }
 
-let statistics = calculateStatistics([5, 3, 100, 3, 9])
+let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
 print(statistics.sum)
 print(statistics.2)
 
-func sumOf(numbers: Int...) -> Int {
-  var sum = 0
-  for number in numbers {
-    sum += number
-  }
-  return sum
-}
-sumOf()
-sumOf(42, 597, 12)
+//func sumOf(numbers: Int...) -> Int {
+//  var sum = 0
+//  for number in numbers {
+//    sum += number
+//  }
+//  return sum
+//}
+//sumOf()
+//sumOf(numbers: 42, 597, 12)
 
 func returnFifteen() -> Int {
- var y = 10
+  var y = 10
   func add() {
     y += 5
   }
@@ -47,7 +52,7 @@ func returnFifteen() -> Int {
 }
 returnFifteen()
 
-func makeIncrementer() -> (Int -> Int) {
+func makeIncrementer() -> ((Int) -> Int) {
   func addOne(number: Int) -> Int {
     return 1 + number
   }
@@ -56,7 +61,7 @@ func makeIncrementer() -> (Int -> Int) {
 var increment = makeIncrementer()
 increment(7)
 
-func hasAnyMatches(list: [Int], condition: Int -> Bool) -> Bool {
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
   for item in list {
     if condition(item) {
       return true
@@ -69,19 +74,17 @@ func lessThanTen(number: Int) -> Bool {
   return number < 10
 }
 var numbers = [20, 19, 7, 12]
-hasAnyMatches(numbers, condition: lessThanTen)
+hasAnyMatches(list: numbers, condition: lessThanTen)
 
 numbers.map { (number: Int) -> Int in
-  let result = 4 * number
+  let result = 3 * number
   return result
 }
 
-let mappedNumbers = numbers.map({ number in
-  3 * number
-})
+let mappedNumbers = numbers.map({ number in 3 * number })
 print(mappedNumbers)
 
-let sortedNumbers = numbers.sort{ $0 > $1 }
+let sortedNumbers = numbers.sorted { $0 > $1 }
 print(sortedNumbers)
 
 //: [Next](@next)

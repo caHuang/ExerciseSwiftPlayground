574668cd82042b22e19fbc8a162e20065e7ca2a5
Yolk Huang <yolk.tw@gmail.com>
2018-12-23 17:09:01 +0800

    Modify Comments page - capitalize the first letter

--- 02 - The Basics.playground/Pages/02 - Comments.xcplaygroundpage/Contents.swift
+++ 02 - The Basics.playground/Pages/02 - Comments.xcplaygroundpage/Contents.swift
@@ -2,13 +2,13 @@
 
 //: Comments
 
-// this is a comment
+// This is a comment.
 
-/* this is also a comment,
- but written over multiple lines */
+/* This is also a comment,
+ but written over multiple lines. */
 
-/* this is the start of the first multiline comment
- /* this is the second, nested multiline comment */
- this is the end of the first multiline comment */
+/* This is the start of the first multiline comment.
+ /* This is the second, nested multiline comment. */
+ This is the end of the first multiline comment. */
 
 //: [Next](@next)

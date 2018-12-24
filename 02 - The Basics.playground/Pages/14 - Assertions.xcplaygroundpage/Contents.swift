119e631407ced4ab8e7d13dc12b2cabd7ac5fe0e
Yolk Huang <yolk.tw@gmail.com>
2018-12-25 01:16:53 +0800

    Update to 4.2 version - Assertions

--- 02 - The Basics.playground/Pages/14 - Assertions.xcplaygroundpage/Contents.swift
+++ 02 - The Basics.playground/Pages/14 - Assertions.xcplaygroundpage/Contents.swift
@@ -1,7 +1,20 @@
 //: [Previous](@previous)
 
 let age = -3
-//assert(age >= 0, "A person's age cannot be less than zero")
-//assert(age >= 0)
+assert(age >= 0, "A person's age can't be less than zero.")
+assert(age >= 0)
+
+if age > 10 {
+  print("You can ride the roller-coaster or the ferris wheel.")
+} else if age > 0 {
+  print("You can ride the ferris wheel.")
+} else {
+  assertionFailure("A person's age can't be less than zero.")
+}
+
+
+let index = -1
+// In the implementation of a subscript...
+precondition(index > 0, "Index must be greater than zero.")
 
 //: [Next](@next)

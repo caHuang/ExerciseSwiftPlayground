2de73c23d2daf0326ddc253d39140eef570aa092
Yolk Huang <yolk.tw@gmail.com>
2018-12-25 23:18:28 +0800

    Update to 4.2 version - Range Operators

--- 03 - Basic Operators.playground/Pages/07 - Range Operators.xcplaygroundpage/Contents.swift
+++ 03 - Basic Operators.playground/Pages/07 - Range Operators.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 //: [Previous](@previous)
 
 for index in 1...5 {
-  print("\(index) * 5 = \(index * 5)")
+  print("\(index) times 5 is \(index * 5)")
 }
 
 let names = ["Anna", "Alex", "Brian", "Jack"]
@@ -9,5 +9,22 @@ let count = names.count
 for i in 0..<count {
   print("Person \(i + 1) is called \(names[i])")
 }
+
+for name in names[2...] {
+  print(name)
+}
+
+for name in names[...2] {
+  print(name)
+}
+
+for name in names[..<2] {
+  print(name)
+}
+
+let range = ...5
+range.contains(7)
+range.contains(4)
+range.contains(-1)
 
 //: [Next](@next)

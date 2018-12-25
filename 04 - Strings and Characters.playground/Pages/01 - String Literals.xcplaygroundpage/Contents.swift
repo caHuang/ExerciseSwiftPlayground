31b0cb2af44134023fe30e63c2d6f6cbb0d3d507
Yolk Huang <yolk.tw@gmail.com>
2018-12-26 00:27:43 +0800

    Update to 4.2 version - Strings Literals

--- 04 - Strings and Characters.playground/Pages/01 - String Literals.xcplaygroundpage/Contents.swift
+++ 04 - Strings and Characters.playground/Pages/01 - String Literals.xcplaygroundpage/Contents.swift
@@ -1,3 +1,47 @@
 let someString = "Some string literal value"
 
+let quotation = """
+The White Rabbit put on his spectacles.  "Where shall I begin,
+please your Majesty?" he asked.
+
+"Begin at the beginning," the King said gravely, "and go on
+till you come to the end; then stop."
+"""
+
+let singleLineString = "These are the same."
+let multilineString = """
+These are the same.
+"""
+
+let softWrappedQuotation = """
+The White Rabbit put on his spectacles.  "Where shall I begin, \
+please your Majesty?" he asked.
+
+"Begin at the beginning," the King said gravely, "and go on \
+till you come to the end; then stop."
+"""
+
+let lineBreaks = """
+
+This string starts with a line break.
+It also ends with a line break.
+
+"""
+
+let linesWithIndentation = """
+    This line doesn't begin with whitespace.
+        This line begins with four spaces.
+    This line doesn't begin with whitespace.
+    """
+
+let wiseWords = "\"Imagination is more important than knowledge\" - Einstein"
+let dollarSign = "\u{24}"        // $,  Unicode scalar U+0024
+let blackHeart = "\u{2665}"      // ♥,  Unicode scalar U+2665
+let sparklingHeart = "\u{1F496}" // 💖, Unicode scalar U+1F496
+
+let threeDoubleQuotationMarks = """
+Escaping the first quotation mark \"""
+Escaping all three quotation marks \"\"\"
+"""
+
 //: [Next](@next)

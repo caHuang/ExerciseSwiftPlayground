31593543299b83c3cb2bd46c5043f5104742c71f
Yolk Huang <yolk.tw@gmail.com>
2018-12-25 23:05:58 +0800

    Update to 4.2 version - Comparison Operators

--- 03 - Basic Operators.playground/Pages/04 - Comparison Operators.xcplaygroundpage/Contents.swift
+++ 03 - Basic Operators.playground/Pages/04 - Comparison Operators.xcplaygroundpage/Contents.swift
@@ -14,4 +14,11 @@ if name == "world" {
   print("I'm sorry \(name), but I don't recognize you")
 }
 
+(1, "zebra") < (2, "apple")
+(3, "apple") < (3, "bird")
+(4, "dog") == (4, "dog")
+
+("blue", -1) < ("purple", 1)
+//("blue", false) < ("purple", true) // Error because < can't compare Boolean values
+
 //: [Next](@next)
